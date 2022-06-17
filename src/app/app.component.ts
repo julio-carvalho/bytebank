b8c9a15906e4664d52ae3bc4ea49daef26f8cbc9
Julio Carvalho <jcarvalhok3@gmail.com>
2022-06-17 15:49:07 -0300

    formatando tabela extrato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'bytebank';
+  title = "bytebank";
+  transferencias: any[] = [];
+
+  transferir($event) {
+    console.log($event);
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia);
+  }
 }
